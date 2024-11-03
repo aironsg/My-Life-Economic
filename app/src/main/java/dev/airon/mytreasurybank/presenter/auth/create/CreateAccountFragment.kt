0775acae8a2d967f1c604ee675b98a8dc184bfb0
Dev Airon <airon_gm@hotmail.com>
2024-11-03 19:07:54 -0300

    conclusão layout telas auth

--- app/src/main/java/dev/airon/mytreasurybank/presenter/auth/create/CreateAccountFragment.kt
+++ app/src/main/java/dev/airon/mytreasurybank/presenter/auth/create/CreateAccountFragment.kt
@@ -1,11 +1,10 @@
-package dev.airon.mytreasurybank.presenter.auth
+package dev.airon.mytreasurybank.presenter.auth.create
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.airon.mytreasurybank.R
 import dev.airon.mytreasurybank.databinding.FragmentCreateAccountBinding
 
 class CreateAccountFragment : Fragment() {
--- app/src/main/java/dev/airon/mytreasurybank/presenter/auth/login/LoginFragment.kt
+++ app/src/main/java/dev/airon/mytreasurybank/presenter/auth/login/LoginFragment.kt
@@ -1,11 +1,10 @@
-package dev.airon.mytreasurybank.presenter.auth
+package dev.airon.mytreasurybank.presenter.auth.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.airon.mytreasurybank.R
 import dev.airon.mytreasurybank.databinding.FragmentLoginBinding
 
 
